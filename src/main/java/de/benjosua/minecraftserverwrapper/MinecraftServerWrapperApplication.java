de601cbc97b755beaeee5cd34776689196874e98
Ben-Josua Lipinski <ben-josua@gmx.de>
2023-01-29 19:38:12 +0100

    Initial commit

---
+++ src/main/java/de/benjosua/minecraftserverwrapper/MinecraftServerWrapperApplication.java
@@ -0,0 +1,13 @@
+package de.benjosua.minecraftserverwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftServerWrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinecraftServerWrapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/benjosua/minecraftserverwrapper/MinecraftServerWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package de.benjosua.minecraftserverwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftServerWrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
