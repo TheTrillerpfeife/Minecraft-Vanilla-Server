5b68d743e19efcbca0720b62a5ec6bb8ecd6a9f0
TheTrillerpfeife <schwiklet@gmx.de>
2023-04-03 22:41:01 +0200

    removed cloudWatch & more logging

--- src/main/java/de/benjosua/minecraftserverwrapper/MinecraftServerWrapperApplication.java
+++ src/main/java/de/benjosua/minecraftserverwrapper/MinecraftServerWrapperApplication.java
@@ -1,5 +1,7 @@
 package de.benjosua.minecraftserverwrapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,27 +11,23 @@ import java.io.IOException;
 @SpringBootApplication
 public class MinecraftServerWrapperApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static Logger logger = LoggerFactory.getLogger(MinecraftServerWrapperApplication.class);
+
+    public static void main(String[] args) {
         SpringApplication.run(MinecraftServerWrapperApplication.class, args);
-        startCloudWatch();
         startMinecraft();
         while (true);
     }
 
     public static void startMinecraft() {
+        logger.info("Starting Minecraft Server");
         ProcessBuilder builder = new ProcessBuilder("java", "-Xms1G", "-Xmx3G", "-jar", "paper.jar", "--nogui");
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
         builder.directory(new File(System.getProperty("user.home") + "/Minecraft"));
         try {
             Process process = builder.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void startCloudWatch() {
-        ProcessBuilder builder = new ProcessBuilder("sudo", "/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl", "-a", "fetch-config", "-m", "ec2", "-s", "-c", "file:" + System.getProperty("user.home") + "/cloud-watch-config.json");
-        try {
-            Process process = builder.start();
+            process.getOutputStream().write("/gamerule playersSleepingPercentage 60".getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
